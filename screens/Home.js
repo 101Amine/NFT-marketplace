39701f3116b2d7c42150da23b9234d3f49769caf
101Amine <aminebecha8@gmail.com>
2022-10-02 10:54:43 +0100

    first commit

---
+++ screens/Home.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import { FlatList, SafeArea, SafeAreaView, View, Text } from "react-native";
+
+import { COLORS, NFTData } from "../constants";
+
+const Home = () => {
+  return <SafeAreaView style={{ flex: 1 }}></SafeAreaView>;
+};
+
+export default Home;
